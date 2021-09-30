1d752296cbb1b9551706f1b83e2e3392fd0a1286
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-09-30 14:55:55 -0600

    starting out project

---
+++ 17-CS/01-Activities/01-Closures/Unsolved/index.js
@@ -0,0 +1,20 @@
+const name = 'Kim';
+
+function person() {
+  const name = 'Lee';
+  return displayName = () => {
+    console.log(name);
+    // return name;
+  };
+
+  // Return a function called `displayName()` with access to the `name` variable
+  // YOUR CODE HERE
+  //
+}
+
+// Invoke `displayName()` in order to print "Lee" in the console
+
+let aPerson = person();
+aPerson();
+// console.log(new person());
+
---
+++ 17-CS/01-Activities/04-Fibonacci/Unsolved/fibonacci.js
@@ -0,0 +1,21 @@
+// Linear Approach
+// const linear = num => {
+//   // Implement the Fibonacci algorithm using the linear approach
+//   // YOUR CODE HERE
+//   //
+// };
+
+// console.log(linear(10)); // => 55
+
+
+let fibonacci_series = function (n) {
+  if (n === 1) {
+    return [0, 1];
+  } else {
+    var s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+console.log(fibonacci_series(20));
